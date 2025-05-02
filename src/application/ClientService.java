0e49e3f49d79ee5d81c2addb1c39190df903ed1d
choihyewon <dongsim00@gmail.com>
2025-05-02 10:37:49 +0900

    feat:matchingResponse 제거 error 추가

--- src/application/ClientService.java
+++ src/application/ClientService.java
@@ -24,7 +24,7 @@ public class ClientService {
     private boolean driving = false;
     //손님 없으면 null로 보내기 위함
     private String currentCustomerLoginId = null;
-    //순서 보장을 위한 수신 했는지에 대한 flag
+    //위치 전송 플래그
     private boolean checkReceive = false;
     //큐 필드 추가 하나으 ㅣ쓰레드가 callqueue를 3초에 한번씩 조회 하나씩 다 검증 배차 됐나 안됐나 ㅋㅋ
     private final BlockingQueue<CallMessageResponse> callQueue = new LinkedBlockingQueue<>();
@@ -35,10 +35,13 @@ public class ClientService {
     //콜 처리 스레드 시작 됨?
     private boolean callConsumerStarted = false;
 
-
+    //재연결 시도
+    private String latestToken;
 
 
     public void connectWithToken(String jwtToken) {
+        this.latestToken = jwtToken;
+
         WebSocketStompClient stompClient = new WebSocketStompClient(new StandardWebSocketClient());
         stompClient.setMessageConverter(new MappingJackson2MessageConverter());
 
@@ -60,7 +63,7 @@ public class ClientService {
                 //택시 상태 수신
                 subscribeTaxiDriverStatus();
 
-                //차 상태 설정
+                //차 상태 설정 - 처음 켰을 때
                 sendTaxiDriverStatus(TaxiDriverStatus.AVAILABLE);
 
                 //위치 전송 스케줄러 시작
@@ -75,16 +78,36 @@ public class ClientService {
                 //콜 거절 결과 수신
                 subscribeRejectCallResult();
 
+                //배차 되었는데 취소당함
+                subscribeDispatchCanceled();
+
 
             }
 
             @Override
             public void handleTransportError(StompSession session, Throwable exception) {
                 System.out.println("WebSocket error: " + exception.getMessage());
+                //재접속
+                tryReconnect();
             }
         });
 
         System.out.println("Connecting to WebSocket server...");
+    }
+
+    //재접속 시도
+    private void tryReconnect() {
+        System.out.println("try reconnect");
+        scheduler.schedule(() -> {
+            try {
+                checkReceive = false;
+                callConsumerStarted = false;
+                System.out.println("reconnecting now");
+                connectWithToken(latestToken);
+            } catch (Exception e) {
+                System.out.println("reconnect fail : " + e.getMessage());
+            }
+        }, 5, TimeUnit.SECONDS);
     }
 
     //택시 상태 수신
@@ -103,7 +126,7 @@ public class ClientService {
                     if (status == TaxiDriverStatus.AVAILABLE) {
                         System.out.println("AVAILABLE");
 
-                        //진심 개거슬리는부분
+                        //진심 개거슬리는부분 - start 함수에서 처리를 하는 게 좋을 거 같다 매번 검사를 하는 건 / 빼내자 실패 하면 종료할 수 있도록
                         //하나로 묶을 방법을 찾고싶음
 
                         if (!checkReceive) {
@@ -123,7 +146,7 @@ public class ClientService {
 
                     } else if (status == TaxiDriverStatus.OFF_DUTY) {
                         System.out.println("OFF_DUTY");
-
+                        //다시 checkReceive 키면 call 받
                         checkReceive = false;
                         // stopSendingLocation();
                     } else {
@@ -136,7 +159,9 @@ public class ClientService {
     }
 
 
-    //off- duty
+    //off - duty
+
+
     //콜 요청 수신
     private void subscribeCallRequest() {
         stompSession.subscribe("/user/queue/call", new StompFrameHandler() {
@@ -189,21 +214,40 @@ public class ClientService {
                     int decision = random.nextInt(10);
                     boolean accept = decision % 2 == 1;  // 홀수면 수락
 
-                    System.out.println("랜덤 처리 번호: " + decision + " → " + (accept ? "수락" : "거절"));
+                    System.out.println("random number : " + decision + " → " + (accept ? "accept" : "reject"));
 
-                    Thread.sleep(1000); // 약간의 대기 (너무 빠르면 서버 감당 힘들 수도)
+                    //보통은 백엔드 개발자는 처리를 할 수 있다는 가정 하에 처리함
+
+                    //자동거절
+                    //client , server -> 기술적으로는 drop / reject 정책을 어떻게 할 지
+
+                    //연결 실패 되거나 연결 되기 전인데 큐 스레드 돌게 될까봐
+                    if(stompSession == null || !stompSession.isConnected()) {
+                        System.out.println("seesion error");
+                        processingCall = false;
+                        continue;
+                    }
+
 
                     if (accept) {
-                        CallAcceptRequest acceptRequest = new CallAcceptRequest();
-                        acceptRequest.setCallId(request.getCallId());
-                        stompSession.send("/api/taxi-driver/accept-call", acceptRequest);
-                        System.out.println("Call accept");
+                        try {
+                            CallAcceptRequest acceptRequest = new CallAcceptRequest();
+                            acceptRequest.setCallId(request.getCallId());
+                            stompSession.send("/api/taxi-driver/accept-call", acceptRequest);
+                            System.out.println("Call accept");
+                        } catch (Exception e) {
+                            System.out.println("call requesting exception : " + e.getMessage());
+                            processingCall = false;
+                        }
                     } else {
-                        CallRejectRequest reject = new CallRejectRequest();
-                        reject.setCallId(request.getCallId());
-                        stompSession.send("/api/taxi-driver/reject-call", reject);
-                        System.out.println("Call rejection");
-
+                        try {
+                            CallRejectRequest reject = new CallRejectRequest();
+                            reject.setCallId(request.getCallId());
+                            stompSession.send("/api/taxi-driver/reject-call", reject);
+                            System.out.println("Call rejection");
+                        } catch (Exception e) {
+                            System.out.println("call reject exception : " + e.getMessage());
+                        }
                         processingCall = false;
                     }
 
@@ -226,10 +270,11 @@ public class ClientService {
             @Override
             public void handleFrame(StompHeaders headers, Object payload) {
                 if (payload instanceof CallAcceptResponse response) {
-                    MatchingResponse matching = response.getMatchingResponse();
-                    if (matching != null) {
-                        currentCustomerLoginId = matching.getCustomerLoginId();
+
+                    if (response.getCustomerLoginId() != null) {
+                        currentCustomerLoginId = response.getCustomerLoginId();
                         driving = true;
+                        //큐 비우기
                         callQueue.clear();
                         processingCall = false;
 
@@ -249,7 +294,19 @@ public class ClientService {
                             driving = false;
                             sendTaxiDriverStatus(TaxiDriverStatus.AVAILABLE);
                         }, 10, TimeUnit.SECONDS);
+
+                        //택시 휴가~
+                        scheduler.schedule(() -> {
+                            System.out.println("off duty");
+                            currentCustomerLoginId = null;
+                            driving = false;
+                            sendTaxiDriverStatus(TaxiDriverStatus.OFF_DUTY);
+                        }, 10, TimeUnit.SECONDS);
                     }
+                } else {
+                    //이미 누가 선점 해버림
+                    System.out.println("accept fail : you lose");
+                    processingCall = false;
                 }
             }
         });
@@ -273,6 +330,31 @@ public class ClientService {
         });
     }
 
+    //배차 했는데 취소당함
+    private void subscribeDispatchCanceled() {
+        stompSession.subscribe("/user/queue/dispatch-canceled", new StompFrameHandler() {
+            @Override
+            public Type getPayloadType(StompHeaders headers) {
+                return ErrorResponse.class;
+            }
+
+            @Override
+            public void handleFrame(StompHeaders headers, Object payload) {
+                if (payload instanceof ErrorResponse response) {
+                    System.out.println("matching cancel : " + response.getMessage());
+
+
+                    currentCustomerLoginId = null;
+                    driving = false;
+                    processingCall = false;
+
+                    sendTaxiDriverStatus(TaxiDriverStatus.AVAILABLE);
+                }
+            }
+        });
+    }
+
+
     //error 구독
 //    user/queue/errors
     private void subscribeErrorMessages() {
@@ -289,13 +371,24 @@ public class ClientService {
         });
     }
 
+    //배차되었는데
+
 
     //차 상태 바꾸기 - 구독 아님
     public void sendTaxiDriverStatus(TaxiDriverStatus status) {
-        UpdateStatus request = new UpdateStatus();
-        request.setStatus(status);
-        stompSession.send("/app/taxi-driver/update-status", request);
-        System.out.println("Send taxi status " + status);
+        if(stompSession == null || !stompSession.isConnected()) {
+            System.out.println("session connecting failed");
+            return;
+        }
+        try {
+            UpdateStatus request = new UpdateStatus();
+            request.setStatus(status);
+            stompSession.send("/app/taxi-driver/update-status", request);
+            System.out.println("Send taxi status " + status);
+        } catch (Exception e) {
+            System.out.println("status sending exception" + e.getMessage());
+        }
+
     }
 
 
@@ -303,7 +396,7 @@ public class ClientService {
     private void startSendingLocation() {
         scheduler.scheduleAtFixedRate(() -> {
             try {
-                //일단은 응답 받으면 checkReceive가 true니깐 그때 받을 수 있게 해둠
+
                 if (stompSession != null && stompSession.isConnected()) {
 
                     if(currentStatus == TaxiDriverStatus.OFF_DUTY) {
@@ -330,7 +423,7 @@ public class ClientService {
     }
 
 
-
+    //에러
 
 
 }
